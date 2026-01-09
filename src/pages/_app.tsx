fe99f89af7778733a1a996e8d920eea4be03352c
unknown <jangjaeko45@gmail.com>
2026-01-08 23:40:46 -0800

    feat : pre-fetching test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,28 @@
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const onClickBtn = () => {
-    router.push("/search");
+    router.push("/test");
     // router.replace("/search");
   };
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href="/">index</Link> |<Link href="/search">search</Link> |{" "}
-        <Link href="/book/1">book/1</Link> |{" "}
+        <Link href="/book/1" prefetch={false}>
+          book/1
+        </Link>{" "}
+        |{" "}
         <div>
-          <button onClick={onClickBtn}>search</button>
+          <button onClick={onClickBtn}>go test</button>
         </div>
       </header>
       <Component {...pageProps} />
