5ccba8b39bb56d7431d0c3f435a615ea3382c238
unknown <jangjaeko45@gmail.com>
2026-01-08 22:45:25 -0800

    feat: navigating setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,22 @@
 import type { AppProps } from "next/app";
-
+import Link from "next/link";
+import { useRouter } from "next/router";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickBtn = () => {
+    router.push("/search");
+    // router.replace("/search");
+  };
+  return (
+    <>
+      <header>
+        <Link href="/">index</Link> |<Link href="/search">search</Link> |{" "}
+        <Link href="/book/1">book/1</Link> |{" "}
+        <div>
+          <button onClick={onClickBtn}>search</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
